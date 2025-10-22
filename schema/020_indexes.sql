fe5b022dd572f8667b7d244ef5dc08ec2474b920
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:18:09 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/020_indexes.sql
+++ schema/020_indexes.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: encrypted_fields
 CREATE INDEX idx_encrypted_fields_field ON encrypted_fields (field_name);
